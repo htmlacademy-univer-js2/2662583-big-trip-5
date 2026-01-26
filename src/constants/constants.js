7d85f7e8ce7120fad414c69d913be98da40950ad
mariya-varlamova <mariya.varlamova.04@mail.ru>
2026-01-26 19:29:20 +0700

    Шаблонизируй это

---
+++ src/constants/constants.js
@@ -0,0 +1,31 @@
+const TYPES = [
+  'taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'
+];
+
+const CITIES = [
+  'Amsterdam', 'Geneva', 'Chamonix', 'Paris', 'London',
+  'Rome', 'Barcelona', 'Berlin', 'Prague', 'Vienna'
+];
+
+const OFFER_TITLES = [
+  'Add luggage', 'Switch to comfort', 'Add meal',
+  'Choose seats', 'Travel by train', 'Order Uber',
+  'Rent a car', 'Book tickets', 'Lunch in city'
+];
+
+const LOREM_IPSUM = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.'
+];
+const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+  'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+export { TYPES, CITIES, OFFER_TITLES, LOREM_IPSUM, months };
